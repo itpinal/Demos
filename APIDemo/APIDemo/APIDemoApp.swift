e00fd49da9c729aca0d6d9dec7a505c2a0893244
Pinal Prajapati <itpinal@gmail.com>
2021-08-29 15:49:26 +0530

    Added Xcode Project for APIDemo

---
+++ APIDemo/APIDemo/APIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIDemoApp.swift
+//  APIDemo
+//
+//  Created by Pinalkumar Prajapati on 29/08/21.
+//
+
+import SwiftUI
+
+@main
+struct APIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
